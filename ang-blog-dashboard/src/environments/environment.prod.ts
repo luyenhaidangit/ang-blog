ba47c4f417b2ca69bd334fdf5ee43da08101bf39
luyenhaidangit <luyenhaidangit@gmail.com>
2023-05-30 15:25:19 +0700

    Connect project to firebase

---
+++ ang-blog-dashboard/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig:{
+        apiKey: "AIzaSyDOrU10DlXb6OPvPA7lLuVcbsPp0vshVTQ",
+        authDomain: "ang-blog-f1b68.firebaseapp.com",
+        projectId: "ang-blog-f1b68",
+        storageBucket: "ang-blog-f1b68.appspot.com",
+        messagingSenderId: "318122023444",
+        appId: "1:318122023444:web:f648753f118ef143716501"
+    }
+  };
+  
