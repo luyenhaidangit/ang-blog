eb957b5d68b79da3ce0e89736ca1ce5cce6dede9
luyenhaidangit <luyenhaidangit@gmail.com>
2023-05-30 09:39:06 +0700

    Complete design dashboard

--- ang-blog-dashboard/src/app/app.module.ts
+++ ang-blog-dashboard/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
