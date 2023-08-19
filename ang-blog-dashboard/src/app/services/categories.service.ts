4f975693d4d629089386c359be5332f8e626f1d9
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2023-08-20 00:11:27 +0700

    Implement feature toastr alert message

--- ang-blog-dashboard/src/app/services/categories.service.ts
+++ ang-blog-dashboard/src/app/services/categories.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(private afs: AngularFirestore, private toastr:ToastrService) { }
 
   saveData(data:any){
     this.afs.collection('categories').add(data).then(docRef => {
       console.log(docRef);
-
+      this.toastr.success('Data insert successfully...!');
     })
     .catch(err=>{console.log(err)})
   }
