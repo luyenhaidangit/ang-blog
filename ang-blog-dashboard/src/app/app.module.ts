dec4deb4b54462515cb21c3a6b84142396ede0b5
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2023-08-20 11:50:13 +0700

    Add angular editor to post form

--- ang-blog-dashboard/src/app/app.module.ts
+++ ang-blog-dashboard/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; 
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AngularEditorModule } from '@kolkov/angular-editor';
 
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
@@ -36,6 +39,8 @@ import { NewPostComponent } from './posts/new-post/new-post.component';
     FormsModule,
     ToastrModule.forRoot(),
     BrowserAnimationsModule,
+    AngularEditorModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
