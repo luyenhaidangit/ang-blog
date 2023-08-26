be0eac9469fb46d18728fdf2f7c9a85e15fe194f
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2023-08-26 19:27:43 +0700

    Implement feature load posts to posts page

--- ang-blog-dashboard/src/app/services/posts.service.ts
+++ ang-blog-dashboard/src/app/services/posts.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor(private storage: AngularFireStorage, private afs:AngularFirestore, private toastr:ToastrService) { }
+  constructor(private storage: AngularFireStorage, private afs:AngularFirestore, private toastr:ToastrService, private router: Router) { }
 
   uploadImage(selectedImage:any,postData:any){
     const filePath = `postIMG/${Date.now()}`;
@@ -19,15 +21,27 @@ export class PostsService {
 
       this.storage.ref(filePath).getDownloadURL().subscribe(URL=>{
         postData.postImgPath = URL;
-      })
 
-      this.saveData(postData);
+        this.saveData(postData);
+      })
     })
   }
 
   saveData(postData:any){
     this.afs.collection('posts').add(postData).then(docRef => {
       this.toastr.success('Data insert successfully');
+
+      this.router.navigate(['/posts']);
     })
+  }
+
+  loadData(){
+    return this.afs.collection('posts').snapshotChanges().pipe(map(actions => {
+      return actions.map(a=>{
+        const data = a.payload.doc.data();
+        const id = a.payload.doc.id;
+        return {id,data}
+      })
+    }));
   }
 }
